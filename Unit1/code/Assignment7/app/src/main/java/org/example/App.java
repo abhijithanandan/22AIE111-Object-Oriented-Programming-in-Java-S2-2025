081f957d3d365aa95f68b7ba8470eae306233135
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2025-01-28 09:25:18 +0530

    added solution for assignment 7

--- Unit1/code/Assignment7/app/src/main/java/org/example/App.java
+++ Unit1/code/Assignment7/app/src/main/java/org/example/App.java
@@ -4,26 +4,16 @@ import java.util.Arrays;
 
 public class App {
 
-    /**
-     * TODO: Swap two integers via 'returning' them in a 2-element array.
-     *
-     * @param a first integer
-     * @param b second integer
-     * @return an array of two elements: [swappedA, swappedB]
-     */
     public int[] swapByValue(int a, int b) {
-        // TODO
-        return new int[]{a, b};
+        // Return them in reversed positions
+        return new int[]{b, a};
     }
 
-    /**
-     * TODO: Swap two integers in-place by reference.
-     * We'll assume the array has exactly 2 elements: arr[0], arr[1].
-     *
-     * @param arr array of two integers
-     */
     public void swapByReference(int[] arr) {
-        // TODO
+        // arr[0] and arr[1] exist
+        int temp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = temp;
     }
 
     public static void main(String[] args) {
@@ -33,11 +23,12 @@ public class App {
         int a = 5, b = 10;
         int[] swapped = app.swapByValue(a, b);
         System.out.println("swapByValue results: " + Arrays.toString(swapped));
+        // a and b remain the same outside this function (5, 10)
 
         // Demonstrate swapByReference
         int[] pair = {5, 10};
         app.swapByReference(pair);
         System.out.println("swapByReference results: " + Arrays.toString(pair));
+        // The original array is modified in-place
     }
 }
-
