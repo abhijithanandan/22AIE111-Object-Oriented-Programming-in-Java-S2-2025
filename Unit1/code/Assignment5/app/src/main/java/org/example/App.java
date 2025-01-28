fe178b2757496d47460415af86ee536b0d57bac3
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2025-01-28 07:12:17 +0530

    added solution for assignment 5

--- Unit1/code/Assignment5/app/src/main/java/org/example/App.java
+++ Unit1/code/Assignment5/app/src/main/java/org/example/App.java
@@ -5,151 +5,82 @@ public class App {
     /* ==============
        ARITHMETIC
        ============== */
-    /**
-     * TODO: Return the sum of two integers.
-     */
     public int add(int a, int b) {
-        // TODO
-        return 0;
+        return a + b;
     }
 
-    /**
-     * TODO: Return the difference of two integers.
-     */
     public int subtract(int a, int b) {
-        // TODO
-        return 0;
+        return a - b;
     }
 
-    /**
-     * TODO: Return the product of two integers.
-     */
     public int multiply(int a, int b) {
-        // TODO
-        return 0;
+        return a * b;
     }
 
-    /**
-     * TODO: Return the integer division result (a / b).
-     * Assume b != 0.
-     */
     public int divide(int a, int b) {
-        // TODO
-        return 0;
+        // integer division (assuming b != 0)
+        return a / b;
     }
 
-    /**
-     * TODO: Return the remainder (modulus) when a is divided by b.
-     * Assume b != 0.
-     */
     public int modulus(int a, int b) {
-        // TODO
-        return 0;
+        return a % b;
     }
 
     /* ==============
        RELATIONAL
        ============== */
-    /**
-     * TODO: Return true if a > b.
-     */
     public boolean greaterThan(int a, int b) {
-        // TODO
-        return false;
+        return a > b;
     }
 
-    /**
-     * TODO: Return true if a == b.
-     */
     public boolean equalTo(int a, int b) {
-        // TODO
-        return false;
+        return a == b;
     }
 
-    /**
-     * TODO: Return true if a <= b.
-     */
     public boolean lessThanOrEqual(int a, int b) {
-        // TODO
-        return false;
+        return a <= b;
     }
 
     /* ==============
        LOGICAL
        ============== */
-    /**
-     * TODO: Return the result of logical AND on x and y.
-     */
     public boolean logicalAnd(boolean x, boolean y) {
-        // TODO
-        return false;
+        return x && y;
     }
 
-    /**
-     * TODO: Return the result of logical OR on x and y.
-     */
     public boolean logicalOr(boolean x, boolean y) {
-        // TODO
-        return false;
+        return x || y;
     }
 
-    /**
-     * TODO: Return the result of logical NOT on x.
-     */
     public boolean logicalNot(boolean x) {
-        // TODO
-        return false;
+        return !x;
     }
 
     /* ==============
        BITWISE
        ============== */
-    /**
-     * TODO: Return the result of bitwise AND on a and b.
-     */
     public int bitwiseAnd(int a, int b) {
-        // TODO
-        return 0;
+        return a & b;
     }
 
-    /**
-     * TODO: Return the result of bitwise OR on a and b.
-     */
     public int bitwiseOr(int a, int b) {
-        // TODO
-        return 0;
+        return a | b;
     }
 
-    /**
-     * TODO: Return the result of bitwise XOR on a and b.
-     */
     public int bitwiseXor(int a, int b) {
-        // TODO
-        return 0;
+        return a ^ b;
     }
 
-    /**
-     * TODO: Return the bitwise complement of a.
-     */
     public int bitwiseNot(int a) {
-        // TODO
-        return 0;
+        return ~a;
     }
 
-    /**
-     * TODO: Return the result of left-shifting a by shift bits.
-     */
     public int leftShift(int a, int shift) {
-        // TODO
-        return 0;
+        return a << shift;
     }
 
-    /**
-     * TODO: Return the result of right-shifting a by shift bits.
-     */
     public int rightShift(int a, int shift) {
-        // TODO
-        return 0;
+        return a >> shift;
     }
 
     public static void main(String[] args) {
@@ -157,32 +88,32 @@ public class App {
 
         // ARITHMETIC
         System.out.println("Arithmetic Examples:");
-        System.out.println("add(10, 3) = " + app.add(10, 3));
-        System.out.println("subtract(10, 3) = " + app.subtract(10, 3));
-        System.out.println("multiply(10, 3) = " + app.multiply(10, 3));
-        System.out.println("divide(10, 3) = " + app.divide(10, 3));
-        System.out.println("modulus(10, 3) = " + app.modulus(10, 3));
+        System.out.println("add(10, 3) = " + app.add(10, 3));           // 13
+        System.out.println("subtract(10, 3) = " + app.subtract(10, 3)); // 7
+        System.out.println("multiply(10, 3) = " + app.multiply(10, 3)); // 30
+        System.out.println("divide(10, 3) = " + app.divide(10, 3));     // 3
+        System.out.println("modulus(10, 3) = " + app.modulus(10, 3));   // 1
 
         // RELATIONAL
         System.out.println("\nRelational Examples:");
-        System.out.println("greaterThan(10, 3) = " + app.greaterThan(10, 3));
-        System.out.println("equalTo(10, 10) = " + app.equalTo(10, 10));
-        System.out.println("lessThanOrEqual(3, 3) = " + app.lessThanOrEqual(3, 3));
+        System.out.println("greaterThan(10, 3) = " + app.greaterThan(10, 3));  // true
+        System.out.println("equalTo(10, 10) = " + app.equalTo(10, 10));        // true
+        System.out.println("lessThanOrEqual(3, 3) = " + app.lessThanOrEqual(3, 3)); // true
 
         // LOGICAL
         System.out.println("\nLogical Examples:");
-        System.out.println("logicalAnd(true, false) = " + app.logicalAnd(true, false));
-        System.out.println("logicalOr(true, false) = " + app.logicalOr(true, false));
-        System.out.println("logicalNot(true) = " + app.logicalNot(true));
+        System.out.println("logicalAnd(true, false) = " + app.logicalAnd(true, false)); // false
+        System.out.println("logicalOr(true, false) = " + app.logicalOr(true, false));   // true
+        System.out.println("logicalNot(true) = " + app.logicalNot(true));               // false
 
         // BITWISE
         System.out.println("\nBitwise Examples:");
-        System.out.println("bitwiseAnd(5, 3) = " + app.bitwiseAnd(5, 3));
-        System.out.println("bitwiseOr(5, 3) = " + app.bitwiseOr(5, 3));
-        System.out.println("bitwiseXor(5, 3) = " + app.bitwiseXor(5, 3));
-        System.out.println("bitwiseNot(5) = " + app.bitwiseNot(5));
-        System.out.println("leftShift(5, 1) = " + app.leftShift(5, 1));
-        System.out.println("rightShift(5, 1) = " + app.rightShift(5, 1));
+        System.out.println("bitwiseAnd(5, 3) = " + app.bitwiseAnd(5, 3)); // 1
+        System.out.println("bitwiseOr(5, 3) = " + app.bitwiseOr(5, 3));   // 7
+        System.out.println("bitwiseXor(5, 3) = " + app.bitwiseXor(5, 3)); // 6
+        System.out.println("bitwiseNot(5) = " + app.bitwiseNot(5));       // -6 (in 32-bit)
+        System.out.println("leftShift(5, 1) = " + app.leftShift(5, 1));   // 10
+        System.out.println("rightShift(5, 1) = " + app.rightShift(5, 1)); // 2
     }
 }
 
